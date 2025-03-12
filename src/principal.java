3658ec4c0123476e3a28086d64cf4bd09ecd9d84
eduardoappel <eduardo.appel@gmail.com>
2025-03-12 08:42:06 -0300

    Procedimentos para calcular o imposto de renda

--- src/principal.java
+++ src/principal.java
@@ -46,6 +46,34 @@ public class principal {
     System.out.print("Informe os Gastos Esducacionais: ");
     gastosEducacional = sc.nextDouble();
 
+    // Cálculo do salário mensal
+    salarioMensal = salarioAnual / 12;
+
+    // Cálculo do imposto sobre o salário
+    if(salarioMensal >= 3000.0 && salarioMensal < 5000.0) {
+        impostoSobreSalario = salarioAnual *0.10;
+    }
+    else if(salarioMensal >= 5000.0){
+        impostoSobreSalario = salarioAnual *0.20;
+    }
+
+    // Cálculo dos impostos sobre outras rendas
+    impostoSobreServico = prestacaoServicoAnual * 0.15;
+    impostoGanhoCapital = ganhoCapitalAnual * 0.20;
+
+    // Cálculo do imposto bruto devido
+    impostoBruto = impostoSobreSalario + impostoSobreServico + impostoGanhoCapital;
+
+     // Cálculo da dedução
+     limiteDeducao = impostoBruto * 0.30;
+     gastosTotais = gastosMedico + gastosEducacional;
+     deducaoPermitida = Math.min(gastosTotais, limiteDeducao);
+
+    // Cálculo do imposto devido
+    impostoDevido = impostoBruto - deducaoPermitida;  
+    
+    
+
     }
 
 }
