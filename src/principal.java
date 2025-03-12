318b6d809dc0340446b111506340bad3f82c09be
eduardoappel <eduardo.appel@gmail.com>
2025-03-12 08:25:36 -0300

    Declaração e inicialização das váriaveis

--- src/principal.java
+++ src/principal.java
@@ -1,7 +1,35 @@
+import java.util.Locale;
+
 public class principal {
 
     public static void main(String[] args) {
         
+    Locale.setDefault(Locale.US);
+    
+    System.out.println("\nDESAFIO: Imposto Java");
+
+    // Declaração e inicialização das váriaveis
+    double salarioAnual = 0.0;
+    double prestacaoServicoAnual = 0.0;
+    double ganhoCapitalAnual = 0.0;
+    double gastosMedico = 0.0;
+    double gastosEducacional = 0.0;
+    
+    double salarioMensal = 0.0;
+
+    double impostoSobreSalario = 0.0;
+    double impostoSobreServico = 0.0;
+    double impostoGanhoCapital = 0.0;
+
+    double impostoBruto = 0.0;
+    double limiteDeducao = 0.0;
+    double gastosTotais = 0.0;
+    double deducaoPermitida = 0.0;
+
+    double impostoDevido = 0.0;
+
+    
+
     }
 
 }
